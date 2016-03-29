7d42c9a5ed2dfc12a0d4b687bcc1deb101a3d01c
resteve <resteve@zikzakmedia.com>
2016-03-30 00:36:55 +0200

    Reactive RST tests scenarios

--- tests/test_account_move_renumber.py
+++ tests/test_account_move_renumber.py
@@ -6,6 +6,7 @@ import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class AccountMoveRenumberTestCase(ModuleTestCase):
@@ -19,5 +20,6 @@ def suite():
         AccountMoveRenumberTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_account_move_renumber.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
