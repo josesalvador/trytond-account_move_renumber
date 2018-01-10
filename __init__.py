6d96528d5e3e3c07c93ef6eb0b7fb6e6fe280ca1
C?dric Krier <ced@b2ck.com>
2018-01-10 15:45:05 +0100

    Fix style

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .account import *
+from . import account
 
 
 def register():
     Pool.register(
-        Move,
-        RenumberMovesStart,
+        account.Move,
+        account.RenumberMovesStart,
         module='account_move_renumber', type_='model')
     Pool.register(
-        RenumberMoves,
+        account.RenumberMoves,
         module='account_move_renumber', type_='wizard')
